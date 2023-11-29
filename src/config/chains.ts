06c16ae25099abd85cc610b1af1d90313d8d4cf5
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2023-11-29 12:52:07 +0300

    refactor: move chains to the config folder

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -1,7 +1,107 @@
-const chains: Record<string, string> = {
-  iotx: '4689',
-  iotxtest: '4690',
-  // TODO: add more chains
-}
+/**
+ * chains.ts
+ *
+ * This file contains the configuration for different blockchain chains.
+ */
 
+import type { ChainInfo } from '~/types'
+
+// Define the chains configuration
+const chainsConfiguration: ChainInfo[] = [
+  {
+    chainId: '4689',
+    chainName: 'IoTeX',
+    shortName: 'iotx',
+    inscriptionPrefix: 'io',
+    description: 'IoTeX Mainnet',
+    rpcUri: {
+      value: 'https://babel-api.mainnet.iotex.io',
+    },
+    publicRpcUri: {
+      value: 'https://babel-api.mainnet.iotex.io',
+    },
+    blockExplorerUriTemplate: {
+      address: 'https://iotexscout.io/address/{{address}}',
+      txHash: 'https://iotexscout.io/tx/{{txHash}}',
+      api: 'https://iotexscout.io/api?module={{module}}&action={{action}}&address={{address}}&apiKey={{apiKey}}',
+    },
+    nativeCurrency: {
+      name: 'IOTX',
+      symbol: 'IOTX',
+      decimals: 18,
+      logoUri: 'https://safe-iotex-static-assets-us-east-1.s3.amazonaws.com/chains/4689/currency_logo.png',
+    },
+    theme: {
+      textColor: '#ffffff',
+      backgroundColor: '#43c9ba',
+    },
+    disabledWallets: ['trust', 'coinbase', 'ledger', 'trezor', 'keystone', 'walletConnect_v2'],
+  },
+  {
+    chainId: '4690',
+    chainName: 'IoTeX Testnet',
+    shortName: 'iotxtest',
+    inscriptionPrefix: 'iott',
+    description: 'IoTeX Testnet',
+    rpcUri: {
+      value: 'https://babel-api.testnet.iotex.io',
+    },
+    publicRpcUri: {
+      value: 'https://babel-api.testnet.iotex.io',
+    },
+    blockExplorerUriTemplate: {
+      address: 'https://testnet.iotexscout.io/address/{{address}}',
+      txHash: 'https://testnet.iotexscout.io/tx/{{txHash}}',
+      api: 'https://testnet.iotexscout.io/api?module={{module}}&action={{action}}&address={{address}}&apiKey={{apiKey}}',
+    },
+    nativeCurrency: {
+      name: 'IOTX',
+      symbol: 'IOTX',
+      decimals: 18,
+      logoUri: 'https://safe-iotex-static-assets-us-east-1.s3.amazonaws.com/chains/4690/currency_logo.png',
+    },
+    theme: {
+      textColor: '#ffffff',
+      backgroundColor: '#000000',
+    },
+    disabledWallets: ['trust', 'coinbase', 'ledger', 'trezor', 'keystone', 'walletConnect_v2'],
+  },
+  {
+    chainId: '17000',
+    chainName: 'Holesky',
+    shortName: 'holesky',
+    inscriptionPrefix: 'hls',
+    description: 'Ethereum Holesky Testnet',
+    nativeCurrency: {
+      name: 'Ether',
+      symbol: 'ETH',
+      decimals: 18,
+      logoUri: 'https://staging-safe-holesky-static-assets-us-east-1.s3.amazonaws.com/chains/17000/currency_logo.png',
+    },
+    blockExplorerUriTemplate: {
+      address: 'https://holesky.etherscan.io/address/{{address}}',
+      txHash: 'https://holesky.etherscan.io/tx/{{txHash}}',
+      api: 'https://holesky.etherscan.io/api?module={{module}}&action={{action}}&address={{address}}&apiKey={{apiKey}}',
+    },
+    disabledWallets: ['trust', 'coinbase', 'ledger', 'trezor', 'keystone', 'walletConnect_v2'],
+    ensRegistryAddress: null,
+    publicRpcUri: {
+      value: 'https://ethereum-holesky.publicnode.com',
+    },
+    rpcUri: {
+      value: 'https://ethereum-holesky.publicnode.com',
+    },
+    theme: {
+      textColor: '#ffffff',
+      backgroundColor: '#000000',
+    },
+  },
+]
+
+// Create a map of chain short names to chain IDs
+const chains: Record<string, string> = chainsConfiguration.reduce((map, chain) => {
+  return { ...map, [chain.shortName]: String(chain.chainId) }
+}, {})
+
+export { chainsConfiguration }
 export default chains
--- src/utils/getChainsConfig.ts
+++ src/utils/getChainsConfig.ts
@@ -1,94 +1,10 @@
 import type { ChainInfo } from '~/types'
+import { chainsConfiguration } from '~/config/chains'
 
+/**
+ * A temporary mock function to get the chains' configuration.
+ * @returns An object with the results of the chain information.
+ */
 export const getChainsConfig = (): { results: ChainInfo[] } => ({
-  results: [
-    {
-      chainId: '4689',
-      chainName: 'IoTeX',
-      shortName: 'iotx',
-      inscriptionPrefix: 'io',
-      description: 'IoTeX Mainnet',
-      rpcUri: {
-        value: 'https://babel-api.mainnet.iotex.io',
-      },
-      publicRpcUri: {
-        value: 'https://babel-api.mainnet.iotex.io',
-      },
-      blockExplorerUriTemplate: {
-        address: 'https://iotexscout.io/address/{{address}}',
-        txHash: 'https://iotexscout.io/tx/{{txHash}}',
-        api: 'https://iotexscout.io/api?module={{module}}&action={{action}}&address={{address}}&apiKey={{apiKey}}',
-      },
-      nativeCurrency: {
-        name: 'IOTX',
-        symbol: 'IOTX',
-        decimals: 18,
-        logoUri: 'https://safe-iotex-static-assets-us-east-1.s3.amazonaws.com/chains/4689/currency_logo.png',
-      },
-      theme: {
-        textColor: '#ffffff',
-        backgroundColor: '#43c9ba',
-      },
-      disabledWallets: ['trust', 'coinbase', 'ledger', 'trezor', 'keystone', 'walletConnect_v2'],
-    },
-    {
-      chainId: '4690',
-      chainName: 'IoTeX Testnet',
-      shortName: 'iotxtest',
-      inscriptionPrefix: 'iott',
-      description: 'IoTeX Testnet',
-      rpcUri: {
-        value: 'https://babel-api.testnet.iotex.io',
-      },
-      publicRpcUri: {
-        value: 'https://babel-api.testnet.iotex.io',
-      },
-      blockExplorerUriTemplate: {
-        address: 'https://testnet.iotexscout.io/address/{{address}}',
-        txHash: 'https://testnet.iotexscout.io/tx/{{txHash}}',
-        api: 'https://testnet.iotexscout.io/api?module={{module}}&action={{action}}&address={{address}}&apiKey={{apiKey}}',
-      },
-      nativeCurrency: {
-        name: 'IOTX',
-        symbol: 'IOTX',
-        decimals: 18,
-        logoUri: 'https://safe-iotex-static-assets-us-east-1.s3.amazonaws.com/chains/4690/currency_logo.png',
-      },
-      theme: {
-        textColor: '#ffffff',
-        backgroundColor: '#000000',
-      },
-      disabledWallets: ['trust', 'coinbase', 'ledger', 'trezor', 'keystone', 'walletConnect_v2'],
-    },
-    {
-      chainId: '17000',
-      chainName: 'Holesky',
-      shortName: 'holesky',
-      inscriptionPrefix: 'hls',
-      description: 'Ethereum Holesky Testnet',
-      nativeCurrency: {
-        name: 'Ether',
-        symbol: 'ETH',
-        decimals: 18,
-        logoUri: 'https://staging-safe-holesky-static-assets-us-east-1.s3.amazonaws.com/chains/17000/currency_logo.png',
-      },
-      blockExplorerUriTemplate: {
-        address: 'https://holesky.etherscan.io/address/{{address}}',
-        txHash: 'https://holesky.etherscan.io/tx/{{txHash}}',
-        api: 'https://holesky.etherscan.io/api?module={{module}}&action={{action}}&address={{address}}&apiKey={{apiKey}}',
-      },
-      disabledWallets: ['trust', 'coinbase', 'ledger', 'trezor', 'keystone', 'walletConnect_v2'],
-      ensRegistryAddress: null,
-      publicRpcUri: {
-        value: 'https://ethereum-holesky.publicnode.com',
-      },
-      rpcUri: {
-        value: 'https://ethereum-holesky.publicnode.com',
-      },
-      theme: {
-        textColor: '#ffffff',
-        backgroundColor: '#000000',
-      },
-    },
-  ],
+  results: chainsConfiguration,
 })
