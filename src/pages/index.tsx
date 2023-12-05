6d793ef49fdcd6e866b6f44d135ade9ce9808e28
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2023-12-06 01:20:44 +0700

    feat: force navigation to the mint page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,22 @@ import Box from '@mui/material/Box'
 
 import Head from 'next/head'
 import { Typography } from '@mui/material'
+import { useEffect, useLayoutEffect } from 'react'
+import { useRouter } from 'next/router'
+import { AppRoutes } from '~/config/routes'
+const useIsomorphicEffect = typeof window !== 'undefined' ? useLayoutEffect : useEffect
 
 export default function Home() {
+  const router = useRouter()
+
+  useIsomorphicEffect(() => {
+    if (router.pathname !== AppRoutes.index) {
+      return
+    }
+
+    void router.replace(AppRoutes.insc721.mint)
+  }, [router])
+
   return (
     <>
       <Head>
