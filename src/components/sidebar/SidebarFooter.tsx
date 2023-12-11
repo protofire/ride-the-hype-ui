7481e984d5de91aea16b6f18c0ff296e257529f8
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2023-12-11 21:34:41 +0700

    feat: add footer and links

--- src/components/sidebar/SidebarFooter.tsx
+++ src/components/sidebar/SidebarFooter.tsx
@@ -1,10 +1,40 @@
 import type { ReactElement } from 'react'
+import { SidebarList, SidebarListItemButton, SidebarListItemText } from '~/components/sidebar/SidebarList'
+import { ListItem } from '@mui/material'
+import { SOCIALS } from '~/config/constants'
+import TwitterIcon from '~/public/images/x-twitter.svg'
+import SvgIcon from '@mui/material/SvgIcon'
+import TelegramIcon from '~/public/images/telegram.svg'
+// import DebugToggle from '../DebugToggle'
 
-import { SidebarList } from '~/components/sidebar/SidebarList'
-
-// TODO: tbd
 const SidebarFooter = (): ReactElement => {
-  return <SidebarList>{null}</SidebarList>
+  return (
+    <SidebarList>
+      {/*{!IS_PRODUCTION && (*/}
+      {/*  <ListItem disablePadding>*/}
+      {/*    <DebugToggle />*/}
+      {/*  </ListItem>*/}
+      {/*)}*/}
+
+      <ListItem disablePadding>
+        <a target="_blank" rel="noopener noreferrer" href={SOCIALS.TWITTER} style={{ width: '100%' }}>
+          <SidebarListItemButton>
+            <SvgIcon component={TwitterIcon} inheritViewBox fontSize="small" sx={{ mr: 1 }} />
+            <SidebarListItemText bold>Twitter</SidebarListItemText>
+          </SidebarListItemButton>
+        </a>
+      </ListItem>
+
+      <ListItem disablePadding>
+        <a target="_blank" rel="noopener noreferrer" href={SOCIALS.TELEGRAM} style={{ width: '100%' }}>
+          <SidebarListItemButton>
+            <SvgIcon component={TelegramIcon} inheritViewBox fontSize="medium" sx={{ mr: 1 }} />
+            <SidebarListItemText bold>Telegram</SidebarListItemText>
+          </SidebarListItemButton>
+        </a>
+      </ListItem>
+    </SidebarList>
+  )
 }
 
 export default SidebarFooter
--- src/config/constants.ts
+++ src/config/constants.ts
@@ -12,3 +12,8 @@ export const DEFAULT_INDEXER_API_BASE_URL = process.env.NEXT_PUBLIC_INDEXER_API_
 // process.env.NEXT_PUBLIC_INDEXER_API_BASE_URL || IS_PRODUCTION
 //   ? 'https://api.ioscriptions.io'
 //   : 'https://inscriptions-api.protofire.io'
+
+export const SOCIALS = {
+  TWITTER: 'https://twitter.com/ioscriptions',
+  TELEGRAM: 'https://t.me/+BQgnkpJ863k4ZmUx',
+}
