2b71bcf91c1c2dddd554ff7f182b8cc1e856c6c8
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2023-12-18 18:01:15 +0700

    feat: add useIntervalCounter hook + polling interval variable

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,11 +1,11 @@
+import * as process from 'process'
+
 export const IS_PRODUCTION = process.env.NEXT_PUBLIC_IS_PRODUCTION === 'true'
-export const LS_NAMESPACE = 'PoC__'
 export const WC_PROJECT_ID = process.env.NEXT_PUBLIC_WC_PROJECT_ID || ''
 
 // TODO: update
 export const TREZOR_APP_URL = 'https://www.blocknative.com'
 export const TREZOR_EMAIL = 'test@test.com'
-export const EMPTY_DATA = '0x'
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
 export const DEFAULT_INDEXER_API_BASE_URL = process.env.NEXT_PUBLIC_INDEXER_API_BASE_URL || ''
@@ -17,3 +17,8 @@ export const SOCIALS = {
   TWITTER: 'https://twitter.com/ioscriptions',
   TELEGRAM: 'https://t.me/+BQgnkpJ863k4ZmUx',
 }
+
+// Magic numbers
+export const POLLING_INTERVAL = Number(process.env.NEXT_PUBLIC_POLLING_INTERVAL) || 15_000
+export const EMPTY_DATA = '0x'
+export const LS_NAMESPACE = 'PoC__'
