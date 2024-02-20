b0a0e6215bd9233cdeb5f96f89cb26c7c7dd3d89
msvstj <savastijonok@gmail.com>
2024-02-20 19:04:36 +0200

    chore: update chains config

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -11,81 +11,6 @@ import { FEATURES } from '~/types'
 // Define the chains configuration
 const chainsConfiguration: ChainInfo[] = [
   {
-    chainId: '10',
-    chainName: 'OP Mainnet',
-    description: 'Optimism Mainnet',
-    shortName: 'oeth',
-    inscriptionPrefix: 'osc',
-    features: [FEATURES.INSC20, FEATURES.CUSTOM_INSC],
-    nativeCurrency: {
-      name: 'Ether',
-      symbol: 'ETH',
-      decimals: 18,
-      logoUri: 'https://assets.coingecko.com/coins/images/279/standard/ethereum.png?1696501628',
-    },
-    blockExplorerUriTemplate: {
-      address: 'https://optimistic.etherscan.io/address/{{address}}',
-      txHash: 'https://optimistic.etherscan.io/tx/{{txHash}}',
-      api: 'https://optimistic.etherscan.io/api?module={{module}}&action={{action}}&address={{address}}&apiKey={{apiKey}}',
-    },
-    disabledWallets: ['trust', 'coinbase', 'ledger', 'trezor', 'keystone', 'walletConnect_v2', 'NONE'],
-    ensRegistryAddress: null,
-    publicRpcUri: {
-      value: 'https://mainnet.optimism.io',
-    },
-    rpcUri: {
-      value: 'https://mainnet.optimism.io',
-    },
-    theme: {
-      textColor: '#ffffff',
-      backgroundColor: '#000000',
-    },
-    apiUri: {
-      value:
-        // process.env.NEXT_PUBLIC_IS_PRODUCTION === 'true'
-        // ? 'https://api.optiscriptions.io/'
-        // :
-        'https://api.stg.optiscriptions.io',
-    },
-    marketplace: '0x688e4229c7FA174eBC990d504522Ab04CBEef6eC',
-  },
-
-  {
-    chainId: '1337',
-    chainName: 'hardhat',
-    description: 'Hardhat localhost',
-    shortName: 'hardhat',
-    inscriptionPrefix: 'hrd',
-    features: [FEATURES.INSC20, FEATURES.CUSTOM_INSC],
-    nativeCurrency: {
-      name: 'Ether',
-      symbol: 'ETH',
-      decimals: 18,
-      logoUri: 'https://assets.coingecko.com/coins/images/279/standard/ethereum.png?1696501628',
-    },
-    blockExplorerUriTemplate: {
-      address: 'https://sepolia-optimism.etherscan.io/address/{{address}}',
-      txHash: 'https://sepolia-optimism.etherscan.io/tx/{{txHash}}',
-      api: 'https://sepolia-optimism.etherscan.io/api?module={{module}}&action={{action}}&address={{address}}&apiKey={{apiKey}}',
-    },
-    disabledWallets: ['trust', 'coinbase', 'ledger', 'trezor', 'keystone', 'walletConnect_v2', 'NONE'],
-    ensRegistryAddress: null,
-    publicRpcUri: {
-      value: 'http://localhost:8545',
-    },
-    rpcUri: {
-      value: 'http://localhost:8545',
-    },
-    theme: {
-      textColor: '#ffffff',
-      backgroundColor: '#000000',
-    },
-    apiUri: {
-      value: 'http://localhost:3001',
-    },
-    marketplace: '0x688e4229c7FA174eBC990d504522Ab04CBEef6eC',
-  },
-  {
     chainId: '168587773',
     chainName: 'Blast Sepolia Testnet',
     description: 'Blast Sepolia Testnet',
@@ -116,9 +41,9 @@ const chainsConfiguration: ChainInfo[] = [
       backgroundColor: '#000000',
     },
     apiUri: {
-      value: 'https://api-test.stg.optiscriptions.io/',
+      value: 'https://api.stg.yieldscriptions.io/.',
     },
-    marketplace: '',
+    marketplace: '0x2d11BA284754A766B4d8De5EB69E9D5ccBbA63a3',
   },
 ]
 
